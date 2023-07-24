867a3583fb5c20beb3b7b6f8016bf28a59333651
AderHenrique <adercontas@gmail.com>
2023-07-24 15:30:28 -0300

    commit final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
-console.log("ola")
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+   const agora = new Date();
+   const timeStampAtual = agora.getTime();
+
+   const distanciaEvento = timeStampDoEvento - timeStampAtual;
+
+   const diaMs = 1000 * 60 * 60 * 24;
+   const horaMs = 1000 * 60 * 60;
+   const minutosMs = 1000 * 60;
+
+   const dias = Math.floor(distanciaEvento / diaMs);
+   const horas = Math.floor((distanciaEvento % diaMs ) / horaMs);
+   const minutos = Math.floor((distanciaEvento% horaMs) / minutosMs);
+   const segundos = Math.floor((distanciaEvento % minutosMs) / 1000);
+
+   document.getElementById('contador').innerHTML = `${dias}d ${horas}h ${minutos}m ${segundos}s`
+
+},1000)
